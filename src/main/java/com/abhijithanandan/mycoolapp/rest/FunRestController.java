414517b73f8cfc5a09cd34a158a7e58a7819eabe
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-12-21 01:32:14 +0530

    Add coach and team properties with injection in controller
    Added `coach.name` and `coach.team` properties to the application.properties file. Updated `FunRestController` to inject these values using `@Value` annotations for dynamic configuration.

--- src/main/java/com/abhijithanandan/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/abhijithanandan/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,19 @@
 package com.abhijithanandan.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Injecting values from properties file
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${coach.team}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
