9fbb5357b585bb7ededcb805ce5181b2684ca64a
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-12-22 03:16:09 +0530

    Add lifecycle hooks and singleton scope to CricketCoach
    Introduced @PostConstruct and @PreDestroy methods for startup and cleanup tasks. Also added @Scope to explicitly define the CricketCoach bean as a singleton. These changes improve lifecycle management and clarify the bean's scope.

--- src/main/java/com/abhijithanandan/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/abhijithanandan/mycoolapp/common/CricketCoach.java
@@ -1,11 +1,26 @@
 package com.abhijithanandan.mycoolapp.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CricketCoach implements Coach {
     @Override
     public String dailyWorkout() {
         return "Practice fast bowling for 15 minutes";
+    }
+
+    @PostConstruct
+    public void doStartupStuff() {
+        System.out.println("I'm doing startup stuff for :" + this.getClass().getName());
+    }
+
+    @PreDestroy
+    public void doCleanupStuff() {
+        System.out.println("I'm doing cleanup stuff for :" + this.getClass().getName());
     }
 }
