61f15307ed05de17acbd405d9b8fafef28bce348
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-12-21 19:17:07 +0530

    Add @Primary annotation to TrackCoach class
    Marked TrackCoach as the primary bean to be injected where Coach interface is used. This ensures that it takes precedence over other implementations of the Coach interface.

--- src/main/java/com/abhijithanandan/mycoolapp/rest/TrackCoach.java
+++ src/main/java/com/abhijithanandan/mycoolapp/rest/TrackCoach.java
@@ -1,8 +1,10 @@
 package com.abhijithanandan.mycoolapp.rest;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TrackCoach implements Coach {
     /**
      * @return String
